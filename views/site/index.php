fbfa7f6a56fb4ab970740a3d29d9414157833bc5
Artem Pakhomov <setnemo@gmail.com>
2019-02-09 20:34:47 +0200

    or no

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations! OR NO!</h1>
+        <h1>Congratulations! OR NO! YEEEEEP!</h1>
 
         <p class="lead">ALL COPS ARE BASTARDS!</p>
 
